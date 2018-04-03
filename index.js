f596598d578d4c2b13aa83882eadf4ea9fcae26b
Kiefer Sivitz <ksivitz@jpg.com>
2018-04-03 14:41:10 -0400

    Complying with eslint

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const path = require("path");
 const repository = packageJson["homepage"] || null;
 const rimraf = require("rimraf");
 
-function pushToGhPages() {
+function pushToGhPages () {
     ghpages.publish("docs", {
             "branch": "master",
             "dest": "docs",
             "repo": repository + ".git"
         },
-        function(error) {
+        function (error) {
             if (error) {
                 console.log("Push to remote failed, please double check that the homepage field in your package.json links to the correct repository.");
                 console.log("The build has completed but has not been pushed to github.");
@@ -29,9 +29,9 @@ function pushToGhPages() {
     );
 }
 
-function copyFiles(originalFile, newFile, callback) {
+function copyFiles (originalFile, newFile, callback) {
     ncp.limit = 16;
-    ncp(originalFile, newFile, function(error) {
+    ncp(originalFile, newFile, function (error) {
         if (error) {
             return console.error(error);
         }
@@ -41,9 +41,9 @@ function copyFiles(originalFile, newFile, callback) {
     });
 }
 
-function editForProduction() {
+function editForProduction () {
     console.log("Preparing files for github pages");
-    fs.readFile("docs/index.html", "utf-8", function(error, data) {
+    fs.readFile("docs/index.html", "utf-8", function (error, data) {
         if (error) {
             return console.error(error);
         }
@@ -55,17 +55,17 @@ function editForProduction() {
     });
 }
 
-function checkIfYarn() {
+function checkIfYarn () {
     return fs.existsSync(path.resolve("./" || process.cwd(), "yarn.lock"));
 }
 
-function runBuild() {
+function runBuild () {
     const packageManagerName = checkIfYarn() ? "yarn" : "npm";
     execSync(`${packageManagerName} run build`, { "stdio": [0, 1, 2] });
-    copyFiles("dist", "docs", function() {
+    copyFiles("dist", "docs", function () {
         console.log("Build Complete.");
         const pathToBuild = "dist";
-        rimraf(pathToBuild, function() {
+        rimraf(pathToBuild, function () {
             const filesToInclude = ["CNAME", "favicon.ico", "404.html"];
             filesToInclude.forEach((file) => {
                 if (fs.existsSync(file)) {
@@ -79,7 +79,7 @@ function runBuild() {
 
 if (fs.existsSync("docs")) {
     const pathToDocs = "docs";
-    rimraf(pathToDocs, function() {
+    rimraf(pathToDocs, function () {
         runBuild();
     });
 } else {
